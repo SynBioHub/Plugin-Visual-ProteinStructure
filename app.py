21936bf67579b59d2fc1adb445a10c093973246c
Mubeena Udyavar Kazi <Mubeena.UdyavarKazi@colorado.edu>
2021-11-18 23:49:16 -0700

    download pdb file  and use PyMOL for rendering.

--- app.py
+++ app.py
@@ -2,6 +2,8 @@ from flask import Flask, request, abort
 import os
 import sys
 import traceback
+import pymol
+import urllib
 
 app = Flask(__name__, static_url_path = "", static_folder = "./")
 
@@ -42,6 +44,7 @@ def evaluate():
 
 @app.route("/run", methods=["POST"])
 def run():
+    print("In Plugin!!!")
     data = request.get_json(force=True)
 
     top_level_url = data['top_level']
@@ -56,14 +59,42 @@ def run():
     cwd = os.getcwd()
     filename = os.path.join(cwd, "Test.html")
 
+    #pdb_filename=["4ogs.pdb", "6hl7.pdb"]
+    #pdb_file_id = int(size)
+    #convert_to_png(pdb_filename[pdb_file_id])
+    pdb_links={'https://dev.synbiohub.org/public/igem/gfp/1':'https://files.rcsb.org/download/4OGS.pdb', 'https://dev.synbiohub.org/public/igem/aTc/1':'https://files.rcsb.org/download/6HL7.pdb'}
+    print("Downloading pdb file: {}".format(pdb_links[url]))
+    urllib.request.urlretrieve(pdb_links[url], "protein.pdb")
+
+    convert_to_png("protein.pdb")
+    
+    print("Created PNG file!")
     try:
         with open(filename, 'r') as htmlfile:
             result = htmlfile.read()
       
         print(result,file=sys.stderr)
+        print("Returning HTML")
         return result
     except Exception as e:
         exc_type, exc_obj, exc_tb = sys.exc_info()
         fname = os.path.split(exc_tb.tb_frame.f_code.co_filename)[1]
         lnum = exc_tb.tb_lineno
         abort(400, f'Exception is: {e}, exc_type: {exc_type}, exc_obj: {exc_obj}, fname: {fname}, line_number: {lnum}, traceback: {traceback.format_exc()}')
+
+def convert_to_png(pdb_filename):
+    if os.path.exists("protein.png"):
+        os.remove("protein.png")
+    pymol.pymol_argv = [ 'pymol', '-qc']
+    #pymol.finish_launching()
+    pdb_file =pdb_filename
+    pdb_name =pdb_filename
+    #pdb_file ="4ogs.pdb"
+    #pdb_name ="4ogs_pdb"
+    pymol.cmd.load(pdb_file, pdb_name)
+    pymol.cmd.disable("all")
+    pymol.cmd.enable(pdb_name)
+    pymol.cmd.png("protein.png")
+    pymol.cmd.delete("all")
+    os.remove(pdb_filename)
+
