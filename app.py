be44d962c1dea6671e5badf1c95a61f7e23cca1b
Mubeena Udyavar Kazi <Mubeena.UdyavarKazi@colorado.edu>
2022-05-09 21:50:09 -0600

    Added protein png image reuse

--- app.py
+++ app.py
@@ -1,5 +1,6 @@
 from flask import Flask, request, abort
 import os
+import subprocess
 import sys
 import traceback
 import xml.etree.ElementTree as ET
@@ -56,6 +57,8 @@ def run():
     cwd = os.getcwd()
     filename = os.path.join(cwd, "result_template.html")
 
+    data_dir = "/data/"
+
     try: 
         subtest_sbol_url = complete_sbol.replace('public/igem/', 'download/sbol_').replace('/1/sbol','.xml') # This is temporary.
         sbol_url = subtest_sbol_url 
@@ -70,24 +73,16 @@ def run():
         print("Attrib = {}".format(sbol_root.attrib),file=sys.stderr)
 
         for sbol_child in sbol_root:
-            if "ComponentDefinition" in sbol_child.tag:
-                for sbol_child_child in sbol_child:
-                    if "pdbId" in sbol_child_child.tag:
-                        print("pdb_id: {} - {}".format(sbol_child_child.tag, sbol_child_child.text),file=sys.stderr)
-                        pdb_id = sbol_child_child.text.lower()
-
-        for sbol_child in sbol_root:
             if "Sequence" in sbol_child.tag:
                 for sbol_child_child in sbol_child:
                     if "elements" in sbol_child_child.tag:
                         print("elements: {} - {}".format(sbol_child_child.tag, sbol_child_child.text),file=sys.stderr)
                         sequence = sbol_child_child.text.lower()
-#TODO: save the sequence and PDB_ID in a text file. Check before using prody. 
-        #f=open('blast_pdb.txt', 'w')
         
+        pdb_list_file = data_dir + "blast_pdb.txt"
         found_pdb_id = False
-        if os.path.exists("blast_pdb.txt"):
-            f=open('blast_pdb.txt', 'r')
+        if os.path.exists(pdb_list_file):
+            f=open(pdb_list_file, 'r')
             for line in f:
                 if sequence in line:
                     print("Found: {}".format(line), file=sys.stderr)
@@ -102,30 +97,45 @@ def run():
             blast_record=blastPDB(sequence)
             best = blast_record.getBest()
             pdb_id = best['pdb_id']
-            f=open('blast_pdb.txt', 'w')      
+            f=open(pdb_list_file, 'w')      
             f.write(sequence + ":" + pdb_id + "\n")
             f.close()
 
         print("Retrieved PDB ID: {}".format(pdb_id), file=sys.stderr)
 
-
-
-
         # Download the pdb file
+        pdb_file_name = "protein_"+pdb_id+".pdb"
         pdb_url_base='https://www.ebi.ac.uk/pdbe/entry-files/download/pdb'
         pdb_file_url = pdb_url_base + pdb_id + '.ent';
         print("Downloading pdb file: {}".format(pdb_file_url), file=sys.stderr)
-        urllib.request.urlretrieve(pdb_file_url, "protein.pdb")
+        urllib.request.urlretrieve(pdb_file_url, pdb_file_name)
+
+        # Check data directory size
+        data_size = subprocess.check_output(['du','-sm', data_dir]).split()[0].decode('utf-8')
+        print("data_size={}".format(data_size), file=sys.stderr)
+        data_files = os.listdir(data_dir)
+        png_files = [os.path.join(data_dir, x) for x in data_files if x.endswith('.png')]
+        if len(png_files) > 0:
+            oldest_png_file = min(png_files, key=os.path.getctime)
+            print("Oldest PNG file: {}".format(oldest_png_file), file=sys.stderr)
+
+            data_limit = 1024 # MiB
+            if int(data_size) >= data_limit:
+                print("Deleting oldest PNG file: {}".format(oldest_png_file), file=sys.stderr)
+                #os.remove(oldest_png_file)
 
         # Get the png image using pymol
-        convert_to_png("protein.pdb")
+        convert_to_png(pdb_id)
     
         print("Created PNG file!", file=sys.stderr)
 
 #        protein_name = complete_sbol.replace(instance_url+'public/igem/', '').replace('/1/sbol', '')
         with open(filename, 'r') as htmlfile:
+            protein_imagename = data_dir + "protein_"+pdb_id+".png"
             result = htmlfile.read()
             result = result.replace("PLUGIN_IP", plugin_ip)
+            result = result.replace("PROTEIN_IMAGEFILE", protein_imagename)
+            
 #            result = result.replace("PLUGIN_PORT", plugin_port)
 #            result = result.replace("PROTEIN_NAME", protein_name)
       
@@ -139,22 +149,20 @@ def run():
         lnum = exc_tb.tb_lineno
         abort(400, f'Exception is: {e}, exc_type: {exc_type}, exc_obj: {exc_obj}, fname: {fname}, line_number: {lnum}, traceback: {traceback.format_exc()}')
 
-def convert_to_png(pdb_filename):
-    if os.path.exists("protein.png"):
-        os.remove("protein.png")
-    #pymol.pymol_argv = [ 'pymol', '-qcy --gldebug']
-    pymol.pymol_argv = [ 'pymol', '-qc']
-    pdb_file =pdb_filename
-    pdb_name =pdb_filename
-    cnt=0;
-    while os.path.exists("protein.png") == False:
-       cnt += 1
-       print("Attempt #{}".format(cnt), file=sys.stderr)
-       pymol.cmd.load(pdb_file, pdb_name)
-       pymol.cmd.disable("all")
-       pymol.cmd.enable(pdb_name)
-       pymol.cmd.png("protein.png")
-       #pymol.cmd.save("protein.png", format='png')
-       pymol.cmd.delete("all")
-    os.remove(pdb_filename)
+def convert_to_png(pdb_id):
+    data_dir = "/data/"
+    protein_imagename = data_dir + "protein_"+pdb_id+".png"
+    pdb_file = "protein_"+pdb_id+".pdb"
+    
+    if not os.path.exists(protein_imagename):
+        print("Converting PDB to PNG ...", file=sys.stderr)
+        pymol.pymol_argv = [ 'pymol', '-qc']
+        pdb_name = "protein_"+pdb_id+".pdb"   
+        pymol.cmd.load(pdb_file, pdb_name)
+        pymol.cmd.disable("all")
+        pymol.cmd.enable(pdb_name)
+        pymol.cmd.png(protein_imagename)
+        pymol.cmd.delete("all")
+
+    os.remove(pdb_file)
 
